55fcc950000f71bf8b20003fa08d9b17f5fa3725
mayank <mayank26202@gmail.com>
2024-08-31 15:53:06 +0530

    food display created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import FoodDisplay from './components/FoodDisplay/FoodDisplay.jsx'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-   <App />
+  <StoreContextProvider>
+    <App />
+  </StoreContextProvider>
   </BrowserRouter>
-   
-  
 )
